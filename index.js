1cc362e6679231adf58d8e45f94279a27bb0e0b5
Avinash Kumar <ak7797405@gmail.com>
2025-01-02 16:12:05 +0530

    updated Index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors")
 const config = require("./config");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
